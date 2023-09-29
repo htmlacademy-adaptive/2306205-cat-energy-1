5ebeff9a8c75d95a92fe1b65d2e1b768e8c3c06f
EugeneChepkasov <evg.chepkasov@gmail.com>
2023-09-29 17:40:25 +0300

    Правка меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,7 @@
 let navToggle = document.querySelector('.main-nav__toggle');
 
-navToggle.classList.remove('main-nav__toggle--no-js');
 navToggle.classList.add('main-nav__toggle--closed');
 
 navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('main-nav__toggle--closed')) {
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
-  } else {
-    navToggle.classList.add('main-nav__toggle--closed');
-    navToggle.classList.remove('main-nav__toggle--opened');
-  }
+    navToggle.classList.toggle('main-nav__toggle--closed');
 });
