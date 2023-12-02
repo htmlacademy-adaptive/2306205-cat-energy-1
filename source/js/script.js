425b0a74fb2fe718cd1185ea13ed22b4a95afb58
EugeneChepkasov <evg.chepkasov@gmail.com>
2023-12-02 20:43:44 +0300

    Правки. Финал

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
+let mainNav = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
+mainNav.classList.remove('main-nav--no-js');
 navToggle.classList.add('main-nav__toggle--closed');
 
 navToggle.addEventListener('click', function () {
